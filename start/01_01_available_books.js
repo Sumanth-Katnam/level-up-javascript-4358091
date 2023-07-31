c15692ecb6350e2ded76936f105b89706ccd3be7
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-07-31 22:17:38 +0000

    01_01

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,39 @@
 // Write your code here
+class Book {
+  constructor(title, author, isbn, numCopies){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  get availability(){
+    return this.getAvailability();
+  }
+
+  getAvailability(){
+    if(this.numCopies === 0){
+      return "out of stock";
+    } else if(this.numCopies < 10){
+      return "low stock";
+    }
+    return "in stock";
+  }
+
+  sell(numSold = 1){
+    this.numCopies = (this.numCopies < numSold)? 0: this.numCopies - numSold;
+  }
+
+  restock(numCopiesStocked = 5){
+    this.numCopies += numCopiesStocked
+  }
+}
+
+const badBatch = new Book("Bad Batch", "Starwars", "98s7adjhis78a", 5);
+console.log(badBatch.getAvailability());
+
+badBatch.restock(10);
+console.log(badBatch.getAvailability());
+
+badBatch.sell(30);
+console.log(badBatch.availability);
