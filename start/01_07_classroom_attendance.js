af487d3bbf5060babd244fe5754e849af7e84adf
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-07-31 23:37:18 +0000

    01_07

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,24 @@
 // Write your code here
+const getStudents = (classroom) => {
+  const {hasTeachingAssistant, classList} = classroom;
+  let teacher, ta, students;
+  if(hasTeachingAssistant){
+    [teacher, ta, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+const class1 = {
+  hasTeachingAssistant: false,
+  classList: ["Prof", "stud1", "stud2"]
+}
+
+const class2 = {
+  hasTeachingAssistant: true,
+  classList: ["Prof", "TA", "stud3", "stud4"]
+}
+
+console.log(getStudents(class1));
+console.log(getStudents(class2));
