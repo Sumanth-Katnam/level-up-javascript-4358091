9452570a1e42ba5a0b95ad1ab7d82d12d09e06ed
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-07-31 23:25:06 +0000

    01_05

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,20 @@
 // Write your code here
+const userName = Symbol("userName");
+const password = Symbol("password");
+
+
+const jack = {
+  userName: "Jack",
+  password: "jackIsBest",
+  age: 25
+}
+
+const secretJack = {
+  [userName]: "Jack",
+  [password]: "jackIsBest",
+  age: 25
+}
+
+
+console.log(`${jack.userName} ${jack.password}`);
+console.log(`${secretJack.userName} ${secretJack.password}`);
