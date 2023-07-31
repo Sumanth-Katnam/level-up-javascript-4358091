faaa24388982175f78f0a09ff3b09a2519923be9
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-07-31 23:59:09 +0000

    01_09

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,10 @@
 // Write your code here
+const calculateTotal = (coffees) => {
+  const total = coffees.reduce((acc, curr) => {
+    acc += curr;
+    return acc;
+  }, 0)
+  console.log(`The total is $${total * 1.25}.`);
+}
+
+calculateTotal([2,3,1,5]) // 13.75
