c856faca67ed79bf53d0ac3209474c39903bd6e4
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-07-31 22:58:05 +0000

    01_02

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,31 @@
 // Write your code here
+// class Movie{
+//   constructor(title, director, genre, releaseYear, rating){
+//     this.title = title ;
+//     this.director = director ;
+//     this.genre = genre ;
+//     this.releaseYear = releaseYear ;
+//     this.rating = rating;
+//   }
+
+//   getoverview(){
+//     console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`);
+//   }
+// }
+
+
+function Movie(title, director, genre, releaseYear, rating){
+  this.title = title ;
+  this.director = director ;
+  this.genre = genre ;
+  this.releaseYear = releaseYear ;
+  this.rating = rating;
+}
+
+Movie.prototype.getoverview = function(){
+  console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`);
+}
+
+const manmadhudu = new Movie("Manmadhudu", "K. Vijaya Bhaskar", "Rom-Com", 2002, 8.3);
+
+manmadhudu.getoverview();
