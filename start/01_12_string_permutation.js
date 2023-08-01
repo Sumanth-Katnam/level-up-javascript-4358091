7a6420c919a66440663d30f90c0f2a444177747f
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-08-01 00:20:46 +0000

    01_12

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,9 @@
 // Write your code here
+const numPerms = (str) => {
+  if(str.length == 1){
+    return 1;
+  }
+  return str.length * numPerms(str.slice(1));
+}
+
+console.log(numPerms("four")) //24
