f9d3c2d6444d914940b913633d993537a3aac2d9
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-08-01 00:36:29 +0000

    01_14

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,7 @@
 // Write your code here
+const urlify = (str) => {
+  const punctuation = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+  return str.toLowerCase().trim().replace(punctuation, "").replaceAll(" ", "-");
+}
+
+console.log(urlify("Some text with punctuations. Also spaces!!"));
