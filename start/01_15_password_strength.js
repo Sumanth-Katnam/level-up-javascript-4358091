0c2d3cadc6266469251fea7e2a9be992268cb86b
Sumanth-Katnam <sumanth.kaatnam@gmail.com>
2023-08-01 00:44:04 +0000

    01_15

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,9 @@
 // Write your code here
+const validatePassword = (passwd) => {
+  const validator = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/g;
+  const isValid = validator.test(passwd);
+  return isValid ? "Valid" : "Invalid";
+}
+
+console.log(validatePassword("kasjhdk"));
+console.log(validatePassword("kasAhd9!"));
